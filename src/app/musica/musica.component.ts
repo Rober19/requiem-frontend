3ec4110895fe737bedd519c572a12301826903a2
Rober19 <texas_telecaster@hotmail.com>
2018-01-27 17:09:04 -0500

    ngif & arrays

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'musica',
-  template: `
-    <h2> {{song}} </h2>
+  template: `    
+
+    <h3 *ngIf="best_song != true" > La Mejor Cancion :{{song_pack.song1}}</h3>
+    <h3 *ngIf="best_song"> La Mejor Cancion :{{song_pack.song2}}</h3>
+
   `
 })
 
 export class musicaComp{
-  public song = 'x & y';
+  public song1 = 'x & y';
+  public song2 = 'amsterdam';
+  public best_song = true;
+
+  public song_pack = {
+    song1: 'x & y',
+    song2: 'amsterdam'
+  }
 }
