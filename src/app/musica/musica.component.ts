2fc5ad0edd726f7824cb7463fc08e092ba861827
Rober19 <texas_telecaster@hotmail.com>
2018-01-29 15:07:28 -0500

    preparing frontend

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'musica',
-  template: `    
-
-    <h3 *ngIf="best_song != true" > La Mejor Cancion :{{song_pack.song1}}</h3>
-    <h3 *ngIf="best_song"> La Mejor Cancion :{{song_pack.song2}}</h3>
-
-  `
+  templateUrl: './musica.components.html'
 })
 
 export class musicaComp{
-  public song1 = 'x & y';
-  public song2 = 'amsterdam';
-  public best_song = true;
+  public song1:string;
+  public song2:string;
+  public best_song:Boolean; 
 
   public song_pack = {
     song1: 'x & y',
     song2: 'amsterdam'
+  }
+
+  public pack:Array<string>;
+
+  constructor(){
+    this.song1 = 'Warning Sign';
+    this.song2 = 'Hardest Part';
+    this.best_song = false;
+    this.pack = [
+      '2',
+      '45',
+      '34',
+      '235'
+    ]
   }
 }
