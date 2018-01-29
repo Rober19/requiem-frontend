56c5f7397eabef8d757bf80bfbe678eb7a6fd3f1
Rober19 <texas_telecaster@hotmail.com>
2018-01-29 17:58:53 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { musicaComp } from './musica/musica.component';
-import { RegisterComponent } from './components/register/register.component'
-import { LoginComponent } from './components/login/login.component'
+
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -15,9 +15,12 @@ import { LoginComponent } from './components/login/login.component'
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
