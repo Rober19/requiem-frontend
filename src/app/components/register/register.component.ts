82682af6ee96c7056e195bbc431c05cf366096be
Rober19 <texas_telecaster@hotmail.com>
2018-01-30 11:56:58 -0500

    fix

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'register',
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
 
 export class RegisterComponent {
