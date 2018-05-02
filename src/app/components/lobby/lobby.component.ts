d96eb450c889ef61833f65b6ce058572c0697f2a
unknown <texas_telecaster@hotmail.com>
2018-05-02 18:23:25 -0500

    paginate 1-5

--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -17,6 +17,9 @@ export class LobbyComponent {
   public UsersArr: Array<any>;
   public UserPages: Array<any>;
   public UserPag: number;
+  public current: number;
+  public initial: number;
+  
 
   constructor(
     private _userService: userService,
@@ -24,30 +27,48 @@ export class LobbyComponent {
     private _router: Router,
   ) {
     this.resMsg = rober19_config.resMsg;
+    this.initial = 0;
+    this.current = 5;
   }
 
   ngOnInit() {
     this.palo = [1, 2, 3, 4];
     this.UserPages = [];
-    
+
     console.log(`LOBBY ${this.resMsg.loaded}`)
 
     this._route.queryParams.subscribe(params => {
       this.UserPag = parseInt(params.pag);
-      this.user_Pages(params.pag)      
+      this.user_Pages(params.pag)
     })
 
-    
+
   }
 
   user_Pages(page) {
     this._userService.getUsers(page).subscribe(data1 => {
       let data: any = data1;
-
-      if (page > data.pages) return this._router.navigateByUrl('/timeline?tab=lobby&pag=1');; 
-      this.UserPages.length = data.pages;            
-      this.UsersArr = data.users;      
+      console.log(data)
+      if (page > data.pages) return this._router.navigateByUrl('/timeline?tab=lobby&pag=1');;
+      this.UserPages.length = data.pages;
+      this.UsersArr = data.users;
     })
+  }
+
+  current5() {
+    if (this.UserPages.length >= this.current) {
+      this.initial += 5;
+      this.current += 5;
+      console.log(this.current)
+    }
+  }
+
+  prev5() {
+    if (this.current>=5) {
+      this.initial -= 5;
+      this.current -= 5;
+      console.log(this.current)
+    }
   }
 
 }
