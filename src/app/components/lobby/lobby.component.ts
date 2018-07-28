7901f7160a7fb4a15b91ccdc021769c0eea36e3e
unknown <texas_telecaster@hotmail.com>
2018-07-27 19:45:21 -0500

    validating lobby component

--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -42,11 +42,11 @@ export class LobbyComponent {
   ngOnInit() {   
     this.UserPages = [];
 
-    console.log(`LOBBY ${this.resMsg.loaded}`)
+    //console.log(`LOBBY ${this.resMsg.loaded}`)
 
     this._route.queryParams.subscribe(params => {
       this.UserPag = parseInt(params.pag);
-      console.log(params)
+      //console.log(params)
       this.user_Pages(params.pag)
     },
   err => {
@@ -63,7 +63,7 @@ export class LobbyComponent {
   user_Pages(page) {
     this._userService.getUsers(page).subscribe(data1 => {
       let data: any = data1;
-      console.log(data)
+      //console.log(data)
       if (page > data.pages) return this._router.navigateByUrl('/timeline?tab=lobby&pag=1');
       this.UserPages.length = data.pages;
       this.UsersArr = data.users;
@@ -86,7 +86,7 @@ export class LobbyComponent {
         this.contentPage++;
       }
 
-      console.log(this.current, ` ${this.UserPages.length}`)
+      //console.log(this.current, ` ${this.UserPages.length}`)
     }
   }
 
