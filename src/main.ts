d8ae3f064c28d9d4dcdcc4d9c165a5c079307359
unknown <texas_telecaster@hotmail.com>
2018-08-04 11:52:59 -0500

    wake up server

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,15 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  setInterval(() => {
+    fetch('https://backend-mean5-project.herokuapp.com/app/get')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(`${myJson} (Wake Up [Server])`);
+      });
+  
+  }, 300000)
+   
